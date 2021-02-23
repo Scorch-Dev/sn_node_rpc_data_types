8cf2fca953ff7f3032293e9a4da74465626a81ef
Alan Armero <scorchdev@gmail.com>
2021-02-23 11:20:16 -0500

    feat: allow automatic serialization of PublicKey/signature/vec<u8> fields

--- src/credentials.rs
+++ src/credentials.rs
@@ -1,11 +1,14 @@
 use serde::{Deserialize, Serialize};
+use serde_with::{hex::Hex, As};
+use sn_data_types::Signature;
 use std::fmt::Debug;
 
 /// Used to pass credentials to issue the rpc
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Credentials {
     /// One time passphrase for the RPC
-    pub passphrase: String,
+    #[serde(with = "As::<Hex>")]
+    pub passphrase: Vec<u8>,
     /// Signature of the passphrase as hex
-    pub signature: String,
+    pub signature: Signature,
 }
--- src/rewards.rs
+++ src/rewards.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use sn_data_types::PublicKey;
 use std::fmt::Debug;
 
 /// Get info regarding rewards, like our reward-key
@@ -11,7 +12,7 @@ pub const METHOD_SET_REWARD_KEY: &str = "set-reward-key";
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GetRewardsInfoResult {
     /// Reward key associated with this node id as hex
-    pub reward_key: String,
+    pub reward_key: PublicKey,
     // TODO: Other info. Current balance, farming stats, etc.
 }
 
@@ -19,12 +20,12 @@ pub struct GetRewardsInfoResult {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SetRewardKeyResult {
     /// the old key you were using
-    pub old_reward_key: String,
+    pub old_reward_key: PublicKey,
 }
 
 /// A struct to hold the parameters of set-reward-key
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SetRewardKeyParams {
     /// The new reward key to use as hex
-    pub reward_key: String,
+    pub reward_key: PublicKey,
 }
