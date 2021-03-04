1b1aa0015c640c037e20f63e88be2b3177c80921
Scorch-Dev <scorchdev@gmail.com>
2021-03-04 06:03:56 -0500

    feat: adds local_stores/node_root to GetStorageInfoResult

--- src/storage.rs
+++ src/storage.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::fmt::Debug;
+use std::{fmt::Debug, path::PathBuf};
 
 /// Get info related to storage on this node (e.g. used vs total offered)
 pub const METHOD_GET_STORAGE_INFO: &str = "get-storage-info";
@@ -7,8 +7,13 @@ pub const METHOD_GET_STORAGE_INFO: &str = "get-storage-info";
 /// Result of a get-storage-info query
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GetStorageInfoResult {
+    /// Root dir of the node itself
+    pub node_root: PathBuf,
     /// Total used storage amount in bytes
     pub used: u64,
     /// Total storage being offered in bytes
     pub total: u64,
+    /// Pairs of root path and value for local stores
+    /// that comprise the used amount
+    pub local_stores: Vec<(PathBuf, u64)>,
 }
